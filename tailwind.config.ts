b42308edc63453a5524e145d8a716f5dec7e0726
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2024-09-27 16:41:04 +0300

    create client side

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,30 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        // Add Arabic and English font families
+        arabic: ['"Tajawal"', '"Cairo"', "sans-serif"],
+        english: ['"Inter"', '"Roboto"', "sans-serif"],
+        poppins: ["var(--font-poppins)"],
+        josefin: ["var(--font-josefin)"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      backgroundImage: {
+        // Adding radial and conic gradients
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      screens: {
+        "2xl": "1536px",
+        xl: "1280px",
+        lg: "1024px",
+        md: "768px",
+        sm: "640px",
+        xs: "480px",
       },
     },
   },
